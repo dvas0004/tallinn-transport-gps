79f43632edd0a3eb4710cd0224929b031ee6ac7f
davevassallo@gmail.com <davevassallo@gmail.com>
2019-07-23 07:32:14 +0300

    updating markers

--- src/components/MapIcons.tsx
+++ src/components/MapIcons.tsx
@@ -2,32 +2,32 @@
 import L from 'leaflet';
 
 const iconBus = new L.Icon({
-    iconUrl: require('../img/bus-marker.png'),
-    iconRetinaUrl: require('../img/bus-marker.png'),
+    iconUrl: require('../img/bus-marker-color.png'),
+    iconRetinaUrl: require('../img/bus-marker-color.png'),
     iconAnchor: new L.Point(15, 45),
     popupAnchor: new L.Point(0, -45),
     iconSize: new L.Point(30, 45)
 });
 
 const iconTram = new L.Icon({
-    iconUrl: require('../img/tram-marker.png'),
-    iconRetinaUrl: require('../img/tram-marker.png'),
+    iconUrl: require('../img/tram-marker-color.png'),
+    iconRetinaUrl: require('../img/tram-marker-color.png'),
     iconAnchor: new L.Point(22, 45),
     popupAnchor: new L.Point(0, -45),
     iconSize: new L.Point(45, 45)
 });
 
 const iconStop = new L.Icon({
-    iconUrl: require('../img/person-marker.png'),
-    iconRetinaUrl: require('../img/person-marker.png'),
+    iconUrl: require('../img/person-marker-color.png'),
+    iconRetinaUrl: require('../img/person-marker-color.png'),
     iconAnchor: new L.Point(22, 45),
     popupAnchor: new L.Point(0, -45),
     iconSize: new L.Point(45, 45)
 });
 
 const iconTrolleybus = new L.Icon({
-    iconUrl: require('../img/trolleybus-marker.png'),
-    iconRetinaUrl: require('../img/trolleybus-marker.png'),
+    iconUrl: require('../img/trolleybus-marker-color.png'),
+    iconRetinaUrl: require('../img/trolleybus-marker-color.png'),
     iconAnchor: new L.Point(22, 45),
     popupAnchor: new L.Point(0, -45),
     iconSize: new L.Point(45, 45)
